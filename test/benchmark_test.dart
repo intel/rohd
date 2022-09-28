e2c96fc34ec94d1aef0303efc6a8f740f9a7ad58
Max Korbel <max.korbel@intel.com>
2022-09-28 14:41:49 -0700

    added testing for benchmarks and made it easier to run them

---
+++ test/benchmark_test.dart
@@ -0,0 +1,18 @@
+/// Copyright (C) 2022 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// benchmark_test.dart
+/// Tests that run benchmarks to make sure they don't break.
+///
+/// 2022 September 28
+/// Author: Max Korbel <max.korbel@intel.com>
+///
+
+import 'package:test/test.dart';
+import '../benchmark/benchmarks.dart';
+
+void main() {
+  test('pipeline benchmark', () {
+    PipelineBenchmark().measure();
+  });
+}
