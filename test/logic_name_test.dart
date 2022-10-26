da797ee074c8b67d1e84869c99df0c5472600f48
Yao Jing Quek <yao.jing.quek@intel.com>
2022-10-26 12:20:23 +0800

    Changed to check for equal value with .name

--- test/logic_name_test.dart
+++ test/logic_name_test.dart
@@ -12,27 +12,18 @@ void main() {
       'GIVEN logic name is valid '
       'THEN expected to see proper name being generated', () async {
     final bus = Logic(name: 'validName');
-    final mod = LogicTestModule(bus);
-    await mod.build();
-    final sv = mod.generateSynth();
-    expect(sv, contains('input logic validName'));
+    expect(bus.name, equals('validName'));
   });
 
   test('GIVEN logic name is invalid THEN expected to see sanitized name',
       () async {
     final bus = Logic(name: '&*-FinvalidN11Me');
-    final mod = LogicTestModule(bus);
-    await mod.build();
-    final sv = mod.generateSynth();
-    expect(sv, contains('___FinvalidN11Me'));
+    expect(bus.name, equals('___FinvalidN11Me'));
   });
 
   test('GIVEN logic name is null THEN expected to see autogeneration of name',
       () async {
     final bus = Logic();
-    final mod = LogicTestModule(bus);
-    await mod.build();
-    final sv = mod.generateSynth();
-    expect(sv, contains('input logic s0'));
+    expect(bus.name, equals('s0'));
   });
 }
