45ba46bb41c810deff5b2a8d477a022553e7f055
Yao Jing Quek <yao.jing.quek@intel.com>
2022-10-26 09:24:43 +0800

    Add SPDX tag and description of file contents

---
+++ lib/src/exceptions/name_exception.dart
@@ -0,0 +1,48 @@
+/// Copyright (C) 2021 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// name_exception.dart
+/// Name Exception that have custom type to be thrown,
+///
+/// 2022 October 25
+/// Author: Yao Jing Quek <yao.jing.quek@intel.com>
+///
+import 'package:rohd/rohd.dart';
+
+/// This Exception show that reserved name eg. definitionName is NULL but
+/// reserve flag eg. reserveDefinitionName is set to True.
+///
+/// Please check on the class [Module] for the constructor argument.
+class NullReservedNameException implements Exception {
+  late final String _message;
+
+  /// constructor for NullReservedNameException,
+  /// pass custom message to the constructor
+  NullReservedNameException(
+      [String message = 'Reserved Name cannot be null '
+          'if reserved name set to true'])
+      : _message = message;
+
+  @override
+  String toString() => _message;
+}
+
+/// This Exception show that reserved name eg. definitionName naming convention
+/// is invalid but reserve flag eg. reserveDefinitionName is set to True.
+/// Please check on the syntax of the reservedName.
+///
+/// Please check on the class [Module] for the constructor argument.
+class InvalidReservedNameException implements Exception {
+  late final String _message;
+
+  /// constructor for InvalidReservedNameException,
+  /// pass custom message to the constructor
+  InvalidReservedNameException(
+      [String message = 'Reserved Name need to follow proper naming '
+          'convention if reserved'
+          ' name set to true'])
+      : _message = message;
+
+  @override
+  String toString() => _message;
+}
