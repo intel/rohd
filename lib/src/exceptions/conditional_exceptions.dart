706a2b00e3092c32799192bbeef557e507719d0f
Yao Jing Quek <yao.jing.quek@intel.com>
2022-11-09 16:43:14 +0800

    Added custom exceptions on conditionals

---
+++ lib/src/exceptions/conditional_exceptions.dart
@@ -0,0 +1,23 @@
+/// Copyright (C) 2022 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// name_exception.dart
+/// Name Exception that have custom type to be thrown,
+///
+/// 2022 November 9
+/// Author: Yao Jing Quek <yao.jing.quek@intel.com>
+///
+import 'package:rohd/rohd.dart';
+
+class SignalRedrivenException implements Exception {
+  late final String _message;
+
+  /// constructor for NullReservedNameException,
+  /// pass custom message to the constructor
+  SignalRedrivenException(String signals,
+      [String message = 'Sequential drove the same signal(s) multiple times: '])
+      : _message = message + signals;
+
+  @override
+  String toString() => _message;
+}
--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,4 +1,5 @@
 /// Copyright (C) 2022 Intel Corporation
 /// SPDX-License-Identifier: BSD-3-Clause
 
+export 'conditional_exceptions.dart';
 export 'name_exceptions.dart';
