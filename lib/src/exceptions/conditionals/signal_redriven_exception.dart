d0b64aa326dbafa5de5486af042518af6de95e56
Yao Jing Quek <yao.jing.quek@intel.com>
2022-11-22 14:52:36 +0800

    Update signal redriven exception documentation

--- lib/src/exceptions/conditionals/signal_redriven_exception.dart
+++ lib/src/exceptions/conditionals/signal_redriven_exception.dart
@@ -7,17 +7,17 @@
 ///
 /// 2022 November 9
 /// Author: Yao Jing Quek <yao.jing.quek@intel.com>
-///
 
 import 'package:rohd/rohd.dart';
 
-/// Throws [SignalRedrivenException] whenever a [Logic] signal
-/// is redriven for mutiple times in [Conditional].
+/// Thrown to indicate a [Logic] signal is operated multiple times.
 class SignalRedrivenException implements Exception {
   late final String _message;
 
-  /// constructor for SignalRedrivenException,
-  /// pass custom String [message] to the constructor to override
+  /// Receives signals that are driven multiple times in string and
+  /// append to the output of exception message.
+  ///
+  /// Creates a [SignalRedrivenException] with an optional error [message].
   SignalRedrivenException(String signals,
       [String message = 'Sequential drove the same signal(s) multiple times: '])
       : _message = message + signals;
