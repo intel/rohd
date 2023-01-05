27ee267025ea44fcc32c5bb4e4dce778115b4cb0
Max Korbel <max.korbel@intel.com>
2023-01-05 14:39:53 -0800

    Fix combinational sensitivity excessive pessimism, fix #233 (#240)

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -2,5 +2,6 @@
 /// SPDX-License-Identifier: BSD-3-Clause
 
 export './conditionals/conditional_exceptions.dart';
+export './module/module_exceptions.dart';
 export './name/name_exceptions.dart';
 export './sim_compare/sim_compare_exceptions.dart';
--- lib/src/exceptions/sim_compare/non_supported_type_exception.dart
+++ lib/src/exceptions/sim_compare/non_supported_type_exception.dart
@@ -9,21 +9,17 @@
 /// Author: Yao Jing Quek <yao.jing.quek@intel.com>
 ///
 
+import 'package:rohd/src/exceptions/rohd_exception.dart';
 import 'package:rohd/src/utilities/simcompare.dart';
 
 /// An exception that thrown when `runtimeType` of expected vector
 /// output from [SimCompare] is invalid or unsupported.
-class NonSupportedTypeException implements Exception {
-  late final String _message;
-
+class NonSupportedTypeException extends RohdException {
   /// Displays [vector] which have invalid or unsupported `runtimeType`
   /// with default error [message].
   ///
   /// Creates a [NonSupportedTypeException] with an optional error [message].
   NonSupportedTypeException(String vector,
       [String message = 'The runtimetype of expected vector is unsupported: '])
-      : _message = message + vector.runtimeType.toString();
-
-  @override
-  String toString() => _message;
+      : super(message + vector.runtimeType.toString());
 }
