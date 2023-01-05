a629fc791994d850e5f998b5e285eae2d6920ce7
Max Korbel <max.korbel@intel.com>
2023-01-05 14:57:03 -0800

    Improve exceptions for Logic.put

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,7 +1,8 @@
-/// Copyright (C) 2022 Intel Corporation
+/// Copyright (C) 2022-2023 Intel Corporation
 /// SPDX-License-Identifier: BSD-3-Clause
-
 export './conditionals/conditional_exceptions.dart';
+export './logic/logic_exceptions.dart';
 export './module/module_exceptions.dart';
 export './name/name_exceptions.dart';
 export './sim_compare/sim_compare_exceptions.dart';
+export 'rohd_exception.dart';
