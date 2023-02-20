0c239c709a9a952d328b652d0d7681a46f69b358
Yao Jing Quek <yao.jing.quek@intel.com>
2023-02-20 14:07:35 +0800

    update the basic logic initialization

--- doc/tutorials/Chapter_2/a_basic_logic.dart
+++ doc/tutorials/Chapter_2/a_basic_logic.dart
@@ -8,30 +8,30 @@
 /// Author: Yao Jing Quek <yao.jing.quek@intel.com>
 ///
 
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_local_variable
 
 import 'package:rohd/rohd.dart';
 import 'helper.dart';
 
 class BasicLogic extends Module {
-  BasicLogic(Logic unamedSignal, Logic busSignal) : super(name: 'BasicLogic') {
-    unamedSignal =
-        addInput('unamed_signal', unamedSignal, width: unamedSignal.width);
-    busSignal = addInput('busSignal', busSignal, width: busSignal.width);
+  BasicLogic() : super(name: 'BasicLogic') {
+    // 1-bit unnamed signal.
+    final unamedSignal = Logic();
+
+    // 8-bit bus named 'b'.
+    final bus = Logic(name: 'b', width: 8);
+
+    // You can use toString() method to check for your signals details.
+    print(unamedSignal.toString());
+
+    // Add ports
+    final signal1 = addInput('', unamedSignal);
+    final signal2 = addInput('b', bus, width: bus.width);
   }
 }
 
 void main() async {
-  // 1-bit unnamed signal.
-  final unamedSignal = Logic();
-
-  // 8-bit bus named 'b'.
-  final bus = Logic(name: 'b', width: 8);
-
-  // You can use toString() method to check for your signals details.
-  print(unamedSignal.toString());
-
   // Instantiate Module and display system verilog
-  final basicLogic = BasicLogic(unamedSignal, bus);
+  final basicLogic = BasicLogic();
   await displaySystemVerilog(basicLogic);
 }
