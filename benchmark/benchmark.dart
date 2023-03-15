55192f9b7d10a89f44662350a4137afa689f7eea
chykon <82272369+chykon@users.noreply.github.com>
2023-03-15 22:53:30 +0300

    Edit `benchmark.dart`

--- benchmark/benchmark.dart
+++ benchmark/benchmark.dart
@@ -1,12 +1,11 @@
-/// Copyright (C) 2022 Intel Corporation
-/// SPDX-License-Identifier: BSD-3-Clause
-///
-/// benchmark.dart
-/// Runs benchmarks and prints values.
-///
-/// 2022 September 28
-/// Author: Max Korbel <max.korbel@intel.com>
-///
+// Copyright (C) 2022-2023 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// benchmark.dart
+// Runs benchmarks and prints values.
+//
+// 2022 September 28
+// Author: Max Korbel <max.korbel@intel.com>
 
 import 'byte_enable_benchmark.dart';
 import 'logic_value_of_benchmark.dart';
