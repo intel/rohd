c97d1216d69e06f544ce224897f6700e01c33726
Yao Jing Quek <yao.jing.quek@intel.com>
2023-03-29 10:47:14 +0800

    Added enum for selection

--- doc/tutorials/chapter_6/combinational_logic.dart
+++ doc/tutorials/chapter_6/combinational_logic.dart
@@ -12,12 +12,15 @@ class FullAdderResult {
   final cOut = Logic(name: 'c_out');
 }
 
+enum FACond { ifblock, caseBlock }
+
 class FullAdder extends Module {
   // Constructor
   FullAdder({
     required Logic a,
     required Logic b,
     required Logic carryIn,
+    FACond selection = FACond.ifblock,
     super.name = 'full_adder',
   }) {
     // Declare Input Node
@@ -29,8 +32,7 @@ class FullAdder extends Module {
     final carryOut = addOutput('carry_out');
     final sum = addOutput('sum');
 
-    const selection = 'case';
-    if (selection == 'if') {
+    if (selection == FACond.ifblock) {
       Combinational([truthTableIf(a, b, carryIn, sum, carryOut)]);
     } else {
       Combinational([truthTableCase(a, b, carryIn, sum, carryOut)]);
@@ -78,46 +80,76 @@ class NBitAdder extends Module {
 }
 
 void main() async {
-  // print(nbitAdder.generateSynth());
-  test('should return true if result sum similar to truth table.', () async {
-    final a = Logic(name: 'a');
-    final b = Logic(name: 'b');
-    final cIn = Logic(name: 'carry_in');
-    final fullAdder = FullAdder(a: a, b: b, carryIn: cIn);
-    await fullAdder.build();
+  group('full adder', () {
+    test(
+        'should return true if result sum similar to truth table when using '
+        'If as conditionals.', () async {
+      final a = Logic(name: 'a');
+      final b = Logic(name: 'b');
+      final cIn = Logic(name: 'carry_in');
 
-    for (var i = 0; i <= 1; i++) {
-      for (var j = 0; j <= 1; j++) {
-        for (var k = 0; k <= 1; k++) {
-          a.put(i);
-          b.put(j);
-          cIn.put(k);
+      final fullAdder = FullAdder(a: a, b: b, carryIn: cIn);
+      await fullAdder.build();
 
-          expect(fullAdder.fullAdderRes.sum.value.toInt(),
-              faTruthTable(i, j, k).sum,
-              reason: 'a: $i, b: $j, c: $k');
+      for (var i = 0; i <= 1; i++) {
+        for (var j = 0; j <= 1; j++) {
+          for (var k = 0; k <= 1; k++) {
+            a.put(i);
+            b.put(j);
+            cIn.put(k);
+
+            expect(fullAdder.fullAdderRes.sum.value.toInt(),
+                faTruthTable(i, j, k).sum,
+                reason: 'a: $i, b: $j, c: $k');
+          }
         }
       }
-    }
+    });
+    test(
+        'should return true if result sum similar to truth table when using '
+        'Case as conditionals.', () async {
+      final a = Logic(name: 'a');
+      final b = Logic(name: 'b');
+      final cIn = Logic(name: 'carry_in');
+
+      final fullAdder =
+          FullAdder(a: a, b: b, carryIn: cIn, selection: FACond.caseBlock);
+      await fullAdder.build();
+
+      for (var i = 0; i <= 1; i++) {
+        for (var j = 0; j <= 1; j++) {
+          for (var k = 0; k <= 1; k++) {
+            a.put(i);
+            b.put(j);
+            cIn.put(k);
+
+            expect(fullAdder.fullAdderRes.sum.value.toInt(),
+                faTruthTable(i, j, k).sum,
+                reason: 'a: $i, b: $j, c: $k');
+          }
+        }
+      }
+    });
   });
 
-  test('should return correct results when nbitadder A and B perform add.',
-      () async {
-    final a = Logic(name: 'a', width: 8);
-    final b = Logic(name: 'b', width: 8);
+  group('nBitAdder', () {
+    test('should return correct results when nbitadder A and B perform add.',
+        () async {
+      final a = Logic(name: 'a', width: 8);
+      final b = Logic(name: 'b', width: 8);
 
-    final nbitAdder = NBitAdder(a, b);
-    await nbitAdder.build();
+      final nbitAdder = NBitAdder(a, b);
+      await nbitAdder.build();
 
-    final randA = Random().nextInt(10);
-    final randB = Random().nextInt(10);
-    final addResult = randA + randB;
+      final randA = Random().nextInt(10);
+      final randB = Random().nextInt(10);
+      final addResult = randA + randB;
 
-    // print("randA: $randA, randB: $randB, addResult: $addResult");
+      a.put(randA);
+      b.put(randB);
 
-    a.put(randA);
-    b.put(randB);
-
-    expect(nbitAdder.sumRes.toInt(), equals(addResult));
+      expect(nbitAdder.sumRes.toInt(), equals(addResult),
+          reason: 'randA: $randA, randB: $randB, addResult: $addResult');
+    });
   });
 }
