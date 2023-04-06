a229135dab4ee86dbf1034bb5f3548ef41cd2182
Rahul Gautham Putcha <rahul.gautham.putcha@intel.com>
2023-04-06 13:15:16 -0400

    counter test for reset flipflops

--- test/conditionals_test.dart
+++ test/conditionals_test.dart
@@ -13,6 +13,35 @@ import 'package:rohd/src/exceptions/sim_compare/sim_compare_exceptions.dart';
 import 'package:rohd/src/utilities/simcompare.dart';
 import 'package:test/test.dart';
 
+import 'counter_wintf_test.dart';
+
+class ResetFlipflops extends Module {
+  late final CounterInterface intf;
+  ResetFlipflops(CounterInterface intf) : super(name: 'shorthandmodule') {
+    this.intf = CounterInterface(intf.width)
+      ..connectIO(this, intf,
+          inputTags: {CounterDirection.inward},
+          outputTags: {CounterDirection.outward});
+
+    // this should do nothing
+    this.intf.connectIO(this, intf);
+
+    _buildLogic();
+  }
+  void _buildLogic() {
+    final nextVal = Logic(name: 'nextVal', width: intf.width);
+
+    nextVal <= intf.val + 1;
+
+    Sequential(
+        SimpleClockGenerator(10).clk,
+        [
+          If(intf.en, then: [intf.val < nextVal])
+        ],
+        reset: intf.reset);
+  }
+}
+
 class ShorthandAssignModule extends Module {
   ShorthandAssignModule(
       Logic preIncr, Logic preDecr, Logic mulAssign, Logic divAssign, Logic b)
@@ -589,6 +618,27 @@ void main() {
         'maOut': 0,
         'daOut': 0,
       })
+    ];
+    await SimCompare.checkFunctionalVector(mod, vectors);
+    final simResult = SimCompare.iverilogVector(mod, vectors);
+    expect(simResult, equals(true));
+  });
+
+  test('resetFlipflop', () async {
+    final mod = ResetFlipflops(CounterInterface(8));
+    await mod.build();
+    final vectors = [
+      Vector({'en': 0, 'reset': 1}, {}),
+      Vector({'en': 0, 'reset': 1}, {'val': 0}),
+      Vector({'en': 1, 'reset': 1}, {'val': 0}),
+      Vector({'en': 1, 'reset': 0}, {'val': 0}),
+      Vector({'en': 1, 'reset': 0}, {'val': 1}),
+      Vector({'en': 1, 'reset': 0}, {'val': 2}),
+      Vector({'en': 1, 'reset': 0}, {'val': 3}),
+      Vector({'en': 0, 'reset': 0}, {'val': 4}),
+      Vector({'en': 0, 'reset': 0}, {'val': 4}),
+      Vector({'en': 1, 'reset': 0}, {'val': 4}),
+      Vector({'en': 0, 'reset': 0}, {'val': 5}),
     ];
     await SimCompare.checkFunctionalVector(mod, vectors);
     final simResult = SimCompare.iverilogVector(mod, vectors);
