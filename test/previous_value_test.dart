58a3c87716f078b41e07a7d9f92f84a355320e94
Max Korbel <max.korbel@intel.com>
2023-06-16 09:34:25 -0700

    fix typo

--- test/previous_value_test.dart
+++ test/previous_value_test.dart
@@ -1,7 +1,7 @@
 // Copyright (C) 2023 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 //
-// previous_value_Test.dart
+// previous_value_test.dart
 // Tests for Logic.previousValue
 //
 // 2023 June 16
