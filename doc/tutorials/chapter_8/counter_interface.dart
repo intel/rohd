a8f7193cbfc0f7f96fe43ebb76f1bdaf5f952dbc
Quek, Yao Jing <yao.jing.quek@intel.com>
2023-08-09 09:13:50 +0800

    Fix bug on the counter interface

--- doc/tutorials/chapter_8/counter_interface.dart
+++ doc/tutorials/chapter_8/counter_interface.dart
@@ -34,35 +34,28 @@ class CounterInterface extends Interface<CounterDirection> {
   }
 }
 
-/// A simple counter which increments once per [clk] edge whenever
-/// [en] is high, and [reset]s to 0, with output [val].
 class Counter extends Module {
-  Logic get clk => input('clk');
-  Logic get en => input('en');
-  Logic get reset => input('reset');
-  Logic get val => output('val');
-
-  late final CounterInterface intf;
+  late final CounterInterface _intf;
 
   Counter(CounterInterface intf) : super(name: 'counter') {
-    this.intf = CounterInterface(width: intf.width)
+    _intf = CounterInterface(width: intf.width)
       ..connectIO(this, intf,
           inputTags: {CounterDirection.inward, CounterDirection.misc},
           outputTags: {CounterDirection.outward});
 
     final nextVal = Logic(name: 'nextVal', width: intf.width);
 
-    nextVal <= intf.val + 1;
+    nextVal <= _intf.val + 1;
 
-    Sequential(intf.clk, [
-      If(intf.reset, then: [
-        intf.val < 0
-      ], orElse: [
-        If(
-          intf.en,
-          then: [intf.val < nextVal],
-        )
-      ])
+    Sequential(_intf.clk, [
+      If.block([
+        Iff(_intf.reset, [
+          _intf.val < 0,
+        ]),
+        ElseIf(_intf.en, [
+          _intf.val < nextVal,
+        ])
+      ]),
     ]);
   }
 }
@@ -70,11 +63,10 @@ class Counter extends Module {
 Future<void> main() async {
   final intf = CounterInterface();
   intf.clk <= SimpleClockGenerator(10).clk;
-
-  final counter = Counter(intf);
-
   intf.en.inject(0);
   intf.reset.inject(1);
+
+  final counter = Counter(intf);
 
   await counter.build();
 
