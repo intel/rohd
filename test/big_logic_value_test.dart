b6410ab54bcc2cf121f76d31bf4529cd44a580fa
Kirkpatrick, Desmond A <desmond.a.kirkpatrick@intel.com>
2023-09-08 13:25:26 -0700

    fixed analysis options

--- test/big_logic_value_test.dart
+++ test/big_logic_value_test.dart
@@ -1,12 +1,3 @@
-/// Copyright (C) 2021-2023 Intel Corporation
-/// SPDX-License-Identifier: BSD-3-Clause
-///
-///
-/// bigint_test.dart
-/// Test of very wide bitvector comparison failure
-///
-
-import 'dart:math';
 import 'package:rohd/rohd.dart';
 import 'package:test/test.dart';
 
@@ -15,7 +6,7 @@ void main() {
     final input = Const(BigInt.from(2).pow(128), width: 129);
     final output = Logic();
     Combinational([
-      IfBlock([
+      If.block([
         Iff(input.getRange(0, 128) > BigInt.from(0),
             [output < Const(1, width: 1)]),
         Else([output < Const(0, width: 1)]),
@@ -27,7 +18,7 @@ void main() {
     final input = Const(BigInt.from(1) << (i - 1), width: i);
     final output = Logic();
     Combinational([
-      IfBlock([
+      If.block([
         Iff(input > BigInt.from(0), [output < Const(1, width: 1)]),
         Else([output < Const(0, width: 1)]),
       ])
