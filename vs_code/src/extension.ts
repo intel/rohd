486b50b7540517fef1303384790951db8047acfe
Quek, Yao Jing <yao.jing.quek@intel.com>
2023-12-14 22:52:41 +0800

    update the message for extension

--- vs_code/src/extension.ts
+++ vs_code/src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rohd" is now active!');
+	console.log('Extension ROHD is now activated!');
 }
 
 
