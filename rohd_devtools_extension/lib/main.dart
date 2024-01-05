f66eba3b35fcd45252227a882e28e4744519ccc2
Quek, Yao Jing <yao.jing.quek@intel.com>
2024-01-05 08:05:29 +0800

    Fix the github action won't run

--- rohd_devtools_extension/lib/main.dart
+++ rohd_devtools_extension/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rohd_devtools_extension/src/modules/tree_structure/view/stack_overflow_ask.dart';
 import 'src/modules/rohd_devtools_module.dart';
 
 void main() {
