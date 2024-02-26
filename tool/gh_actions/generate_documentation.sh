8221681a467e501d92dac897b5f3b658975f65da
Max Korbel <max.korbel@intel.com>
2024-02-26 11:42:13 -0800

    Fix documentation generation

--- tool/gh_actions/generate_documentation.sh
+++ tool/gh_actions/generate_documentation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2022-2023 Intel Corporation
+# Copyright (C) 2022-2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # generate_documentation.sh
@@ -11,15 +11,21 @@
 
 set -euo pipefail
 
-# See script "check_documentation.sh" for a note on processing "dart doc" output.
-
-# The documentation will be placed in the "doc/api" folder.
+# Output parsing is required because "dart doc" is not capable of
+# signaling a warning with an exit code:
+#   https://github.com/dart-lang/dartdoc/issues/2846
+#   https://github.com/dart-lang/dartdoc/issues/2907
+#   https://github.com/dart-lang/dartdoc/issues/1959
 
 # Disabling --validate-links due to https://github.com/dart-lang/dartdoc/issues/3584
 # output=$(dart doc --validate-links 2>&1 | tee)
 output=$(dart doc 2>&1 | tee)
 
-echo "${output}"
-
 # In case of problems, the searched substring will not be found.
-echo "${output}" | grep --silent 'no issues found'
+if echo "${output}" | grep --silent -e 'no issues found' -e 'Success!'; then
+  echo 'Documentation check passed!'
+else
+  echo "${output}"
+  echo 'Documentation failed since some issues were found'
+  exit 1
+fi
