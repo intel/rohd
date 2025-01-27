808dada4a5cd021ad47bc086cd7da83325d87efa
Max Korbel <max.korbel@intel.com>
2025-01-27 11:51:55 -0800

    Support VCDs generated by Verilator in VcdParser (#557)

--- test/vcd_parser_test.dart
+++ test/vcd_parser_test.dart
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 Intel Corporation
+// Copyright (C) 2023-2025 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 //
 // vcd_parser_test.dart
@@ -20,6 +20,24 @@ void main() {
   test('VCD parser can parse Icarus Verilog generated VCD file', () {
     final vcdContents =
         File('test/example_icarus_waves.vcd').readAsStringSync();
+    for (var countI = 1; countI < 10; countI++) {
+      expect(
+        VcdParser.confirmValue(
+          vcdContents,
+          'sampled',
+          (10 * countI + 5) * 1000,
+          LogicValue.ofInt(countI.isEven ? countI - 2 : countI - 1, 8),
+        ),
+        isTrue,
+      );
+    }
+  });
+
+  test('VCD parser can parse Verilator generated VCD file', () {
+    // NOTE: this one is extra interesting since initial values matter and it
+    // doesn't have a dumpvars section
+    final vcdContents =
+        File('test/example_verilator_waves.vcd').readAsStringSync();
     for (var countI = 1; countI < 10; countI++) {
       expect(
         VcdParser.confirmValue(
