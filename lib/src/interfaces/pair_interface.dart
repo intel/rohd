41271110104ed5eae5bf35e01d57bee771bd5618
Korbel, Max <max.korbel@intel.com>
2025-08-27 15:28:59 -0700

    Deprecate PairInterface modify in favor of uniquify on subInterfaces

--- lib/src/interfaces/pair_interface.dart
+++ lib/src/interfaces/pair_interface.dart
@@ -45,6 +45,8 @@ enum PairRole {
 /// complex situations.
 class PairInterface extends Interface<PairDirection> {
   /// A function that can be used to modify all port names in a certain way.
+  @Deprecated(
+      'Use `uniquify` when connecting or adding sub interfaces instead.')
   String Function(String original)? modify;
 
   /// Constructs an instance of a [PairInterface] with the specified ports.
@@ -56,6 +58,8 @@ class PairInterface extends Interface<PairDirection> {
     List<Logic>? portsFromProvider,
     List<Logic>? sharedInputPorts,
     List<Logic>? commonInOutPorts,
+    @Deprecated(
+        'Use `uniquify` when connecting or adding sub interfaces instead.')
     this.modify,
   }) {
     if (portsFromConsumer != null) {
@@ -184,6 +188,12 @@ class PairInterface extends Interface<PairDirection> {
         final subInterface = subInterfaceEntry.value.interface;
         final subInterfaceName = subInterfaceEntry.key;
 
+        final nonNullSubIntfUniquify =
+            subInterfaceEntry.value.uniquify ?? (original) => original;
+
+        String newSubIntfUniquify(String original) =>
+            nonNullUniquify(nonNullSubIntfUniquify(nonNullModify(original)));
+
         if (!srcInterface._subInterfaces.containsKey(subInterfaceName)) {
           throw InterfaceTypeException(
               srcInterface, 'missing a sub-interface named $subInterfaceName');
@@ -229,7 +239,7 @@ class PairInterface extends Interface<PairDirection> {
           inputTags: subIntfInputTags,
           outputTags: subIntfOutputTags,
           inOutTags: subIntfInOutTags,
-          uniquify: newUniquify,
+          uniquify: newSubIntfUniquify,
         );
       }
     }
@@ -254,6 +264,7 @@ class PairInterface extends Interface<PairDirection> {
     String name,
     PairInterfaceType subInterface, {
     bool reverse = false,
+    String Function(String original)? uniquify,
   }) {
     if (_subInterfaces.containsKey(name)) {
       throw InterfaceNameException(
@@ -266,7 +277,11 @@ class PairInterface extends Interface<PairDirection> {
       throw InterfaceNameException(name, 'Sub-interface name is not sanitary.');
     }
 
-    _subInterfaces[name] = _SubPairInterface(subInterface, reverse: reverse);
+    _subInterfaces[name] = _SubPairInterface(
+      subInterface,
+      reverse: reverse,
+      uniquify: uniquify,
+    );
     return subInterface;
   }
 
@@ -285,6 +300,8 @@ class _SubPairInterface<PairInterfaceType extends PairInterface> {
   /// Whether or not this interface should be connected in a reverse way.
   final bool reverse;
 
+  final String Function(String original)? uniquify;
+
   /// Constructs a new sub-interface tracking object with characteristics.
-  _SubPairInterface(this.interface, {required this.reverse});
+  _SubPairInterface(this.interface, {required this.reverse, this.uniquify});
 }
---
+++ test/pair_interface_hier_w_modify_test.dart
@@ -0,0 +1,101 @@
+// Copyright (C) 2023-2025 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// pair_interface_hier_w_modify_test.dart
+// Tests for PairInterface with hierarchy (testing deprecated modify)
+//
+// 2023 March 9
+// Author: Max Korbel <max.korbel@intel.com>
+
+import 'package:rohd/rohd.dart';
+import 'package:test/test.dart';
+
+class SubInterface extends PairInterface {
+  Logic get req => port('req');
+  Logic get rsp => port('rsp');
+  Logic get io => port('io');
+  LogicArray get ioArr => port('io_arr') as LogicArray;
+
+  SubInterface({super.modify})
+      : super(
+          portsFromConsumer: [Logic.port('rsp')],
+          portsFromProvider: [LogicArray.port('req')],
+          commonInOutPorts: [
+            LogicNet.port('io'),
+            LogicArray.netPort('io_arr', [3])
+          ],
+        );
+
+  @override
+  SubInterface clone() => SubInterface(modify: modify);
+}
+
+class TopLevelInterface extends PairInterface {
+  Logic get clk => port('clk');
+
+  final int numSubInterfaces;
+
+  final List<SubInterface> subIntfs = [];
+
+  TopLevelInterface(this.numSubInterfaces)
+      : super(
+          sharedInputPorts: [Logic.port('clk')],
+        ) {
+    for (var i = 0; i < numSubInterfaces; i++) {
+      subIntfs.add(addSubInterface(
+          'sub$i',
+          SubInterface(
+            modify: (original) => '${original}_$i',
+          )));
+    }
+  }
+
+  @override
+  TopLevelInterface clone() => TopLevelInterface(numSubInterfaces);
+}
+
+class HierProducer extends Module {
+  late final TopLevelInterface _intf;
+  HierProducer(TopLevelInterface intf) {
+    _intf = intf.clone()..pairConnectIO(this, intf, PairRole.provider);
+
+    _intf.subIntfs[0].req <= FlipFlop(_intf.clk, _intf.subIntfs[0].rsp).q;
+  }
+}
+
+class HierConsumer extends Module {
+  late final TopLevelInterface _intf;
+  HierConsumer(TopLevelInterface intf) {
+    _intf = intf.clone()..pairConnectIO(this, intf, PairRole.consumer);
+
+    _intf.subIntfs[1].rsp <= FlipFlop(_intf.clk, _intf.subIntfs[1].req).q;
+  }
+}
+
+class HierTop extends Module {
+  HierTop(Logic clk) {
+    clk = addInput('clk', clk);
+    final intf = TopLevelInterface(3);
+    intf.clk <= clk;
+    HierConsumer(intf);
+    HierProducer(intf);
+  }
+}
+
+void main() {
+  tearDown(() async {
+    await Simulator.reset();
+  });
+
+  test('hier pair interface', () async {
+    final mod = HierTop(Logic());
+    await mod.build();
+
+    final sv = mod.generateSynth();
+
+    expect(sv, contains('HierConsumer  unnamed_module'));
+    expect(sv, contains('HierProducer  unnamed_module'));
+    expect(sv, contains('inout wire io_0'));
+    expect(sv, contains('inout wire [2:0] io_arr_0'));
+  });
+}
