19df6f4f84f4881942728f72ca21a74f5ce4c794
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2026-07-02 11:06:07 -0700

    Use SvService in simcompare

--- lib/src/utilities/simcompare.dart
+++ lib/src/utilities/simcompare.dart
@@ -327,7 +327,7 @@ abstract class SimCompare {
         allSignals.map((e) => '.$e(${logicToWireMapping[e] ?? e})').join(', ');
     final moduleInstance = '$topModule dut($moduleConnections);';
     final stimulus = vectors.map((e) => e.toTbVerilog(module)).join('\n');
-    final generatedVerilog = module.generateSynth();
+    final generatedVerilog = SvService(module, register: false).synthOutput;
 
     // so that when they run in parallel, they dont step on each other
     final uniqueId =
